d238b8c35022bc79b10bb306d46d4ec11624503c
denisatop345 <denisatopolicianu908@gmail.com>
2025-12-02 23:41:26 +0200

    Update script.js

--- Topolicianu_Denisa/script.js
+++ Topolicianu_Denisa/script.js
@@ -1,28 +1,20 @@
 var slides = ["cpu.jpg", "ram.jpg", "disk.jpg"];
 var index = 0;
 var slideshowStarted = false;
-
-// Schimbă imaginea la 3 secunde
 function changeSlide() {
     index++;
     if (index >= slides.length) index = 0;
     document.getElementById("slideimg").src = slides[index];
 }
-
-// Eveniment dublu-click pe titlul secțiunii
 document.getElementById("monitorizare-title").ondblclick = function() {
 
     if (!slideshowStarted) {
-
-        // Ascundem imaginile statice
         document.getElementById("static-images").style.display = "none";
 
-        // Afișăm slideshow-ul
         document.getElementById("slideshow").style.display = "block";
-
-        // Pornim slideshow-ul
         setInterval(changeSlide, 3000);
 
         slideshowStarted = true;
     }
 }
+
